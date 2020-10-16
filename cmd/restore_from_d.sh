472a88091670795fd8b9e8c6e4d04dfc03c2601c
Ben <disanben@gmail.com>
2020-10-16 11:26:34 +0200

    Creato nuovo file cmd per restore da disco D su CygDrive

---
+++ cmd/restore_from_d.sh
@@ -0,0 +1,4 @@
+mv /home/ConsDiSanoBenedetto/migrazione /home/ConsDiSanoBenedetto/orig_migrazione
+cp -r /cygdrive/d/doc/progetti/tool_migrazioni/migrazione/migrazioni_sia/  /home/ConsDiSanoBenedetto/
+mv /home/ConsDiSanoBenedetto/migrazioni_sia /home/ConsDiSanoBenedetto/migrazione
+echo "Copy Done"
